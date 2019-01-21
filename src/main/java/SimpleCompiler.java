9fad045ca12fffea0d0b9a9a72413db9458ab900
Ajay MT <ajay.tatachar@gmail.com>
2019-01-21 02:07:09 -0600

    remove unnecessary class parsing functions.

--- src/main/java/SimpleCompiler.java
+++ src/main/java/SimpleCompiler.java
@@ -8,11 +8,6 @@ import java.lang.reflect.Modifier;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-import java.util.regex.Pattern;
 
 /**
  * Run a class method using Janino.
@@ -58,108 +53,14 @@ public class SimpleCompiler extends Source {
     }
 
     /**
-     * Extract the name of a class from a class declaration token.
-     *
-     * @param token the token to parse
-     * @return the name of the class being declared
-     */
-    private String parseClassDeclaration(final String token) {
-        return token.split(Pattern.quote("{"))[0].split(Pattern.quote("<"))[0];
-    }
-
-    /**
-     * Parse a single string of code and split it into individual classes.
-     *
-     * @param source the source code to parse
-     * @return a map of class names to class code within source
-     */
-    private Map<String, String> parseClasses(final String source) {
-        final List<String> accessModifiers = Arrays.asList(new String[] {
-                "public", "private", "protected"
-            });
-
-        Map<String, String> classes = new HashMap<>();
-        String[] lines = source.split("\\r?\\n");
-        List<Integer> lineNumbers = new ArrayList<>();
-        List<Integer> packageLineNumbers = new ArrayList<>();
-        String packageSource = "";
-
-        // construct list of line numbers of class declarations
-        for (int lineNumber = 0; lineNumber < lines.length; ++lineNumber) {
-            String line = new String(lines[lineNumber]);
-            line.trim();
-            List<String> tokens = Arrays.asList(line.split(" "))
-                .stream()
-                .filter(s -> s.length() > 0)
-                .collect(Collectors.toList());
-
-            if (tokens.size() == 0) {
-                continue;
-            }
-
-            boolean isClass = tokens.get(0).equals("class");
-            if (tokens.size() > 1) {
-                isClass = isClass
-                    || (tokens.get(1).equals("class")
-                        && accessModifiers.contains(tokens.get(0)));
-            }
-
-            boolean isPackageLine = tokens.get(0).equals("import")
-                || tokens.get(0).equals("package");
-
-            if (isClass) {
-                lineNumbers.add(lineNumber);
-            } else if (isPackageLine) {
-                packageLineNumbers.add(lineNumber);
-            }
-        }
-
-        // assemble package lines (i.e import and package statements)
-        for (int lineNumber : packageLineNumbers) {
-            packageSource += lines[lineNumber] + "\n";
-        }
-
-        // construct map of class names to class source code
-        for (int lineIndex = 0; lineIndex < lineNumbers.size(); ++lineIndex) {
-            int current = lineNumbers.get(lineIndex);
-            int next = lines.length;
-
-            if (lineIndex < lineNumbers.size() - 1) {
-                next = lineNumbers.get(lineIndex + 1);
-            }
-
-            String classSource = String.join("\n", Arrays.copyOfRange(lines, current, next));
-            String classNameLocal = "";
-            List<String> tokens = Arrays.asList(lines[current].split(" "))
-                .stream()
-                .filter(s -> s.length() > 0)
-                .collect(Collectors.toList());
-
-            for (int tokenIndex = 0; tokenIndex < tokens.size() - 1; ++tokenIndex) {
-                if (tokens.get(tokenIndex).equals("class")) {
-                    classNameLocal = parseClassDeclaration(tokens.get(tokenIndex + 1));
-                }
-            }
-
-            classes.put(classNameLocal, packageSource + classSource);
-        }
-
-        return classes;
-    }
-
-    /**
      * Get a map of sources for this snippet. FIXME.
      *
      * @return a map of sources for this snippet.
      */
     protected Map<String, String> sources() {
         Map<String, String> classSources = new HashMap<>();
-
-        for (String source : sources) {
-            Map<String, String> classes = parseClasses(source);
-            for (Map.Entry<String, String> klass : classes.entrySet()) {
-                classSources.put(klass.getKey() + ".java", klass.getValue());
-            }
+        for (int i = 0; i < sources.length; ++i) {
+            classSources.put(Integer.toString(i) + ".java", sources[i]);
         }
 
         return classSources;
